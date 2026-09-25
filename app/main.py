50fb4102748bda75eb01a6f943944fde379c9064
BagrinD <bagrin.danu@gmail.com>
2026-09-25 18:36:04 +0300

    init proj

---
+++ app/main.py
@@ -0,0 +1,21 @@
+import logging
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse, HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("verdikt")
+
+app = FastAPI(
+    title="Test",
+    version="1.0.0",
+    description="DeepDeckGigaByte"
+)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=12000, reload=True)
